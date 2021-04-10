434995b8bc55e8c619b6ee95a28747a3815cfdca
HaYeonKim0807 <kh701201@naver.com>
2021-04-10 15:39:43 +0900

    Hello HaYeon

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sort
+{
+    class Program
+    {
+        class Monster
+        {
+            public int id;
+
+            public Monster(int id) { this.id = id; }    
+        }
+
+
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+
+
+            Dictionary<int, Monster> dic = new Dictionary<int, Monster>();
+           for ( int i = 0; i < 10000; i++)
+            {
+                dic.Add(i, new Monster(i));
+            }
+            Monster mon;
+
+            bool F = dic.TryGetValue(7777, out mon);
+
+            dic.Remove(7777);
+            dic.Clear();
+        
+        }
+    }
+}
